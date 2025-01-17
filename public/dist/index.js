5242af55cf58394c06122e3f94c66a30599b19c3
Letícia Vitória <leticia.perez2145@gmail.com>
2025-01-17 10:25:05 -0300

    Iniciando o projeto

---
+++ public/dist/index.js
@@ -0,0 +1,35 @@
+"use strict";
+(function () {
+    var todo = {
+        description: 'todo',
+        done: false,
+    };
+    var reminder = {
+        description: 'reminder',
+        date: '15.12.2021',
+    };
+    var taskView = {
+        render: function (tasks) {
+            var tasksList = document.getElementById('tasksList');
+            while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement('LI');
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li === null || li === void 0 ? void 0 : li.appendChild(textNode);
+                tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
+            });
+        },
+    };
+    var TaskController = function (view) {
+        var _a;
+        var tasks = [todo, reminder];
+        var handleEvent = function (event) {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document.getElementById('taskForm')) === null || _a === void 0 ? void 0 : _a.addEventListener('submit', handleEvent);
+    };
+    TaskController(taskView);
+})();
